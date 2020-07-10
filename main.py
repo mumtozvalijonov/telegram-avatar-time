604b82bf131583e4a9aba1f316ad222a86542c56
Mumtoz Valijonov <mumtozvalijonov@gmail.com>
2020-07-11 01:23:24 +0500

    tz, api_id, api_hash as argparse

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import time
-
 from telethon import TelegramClient
-
 from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
-
 from config import api_hash, api_id
 from utils import time_has_changed, generate_time_image_bytes
 from datetime import datetime, timedelta
+import argparse
+import pytz
 
 
-client = TelegramClient("carpediem", api_id, api_hash)
+def valid_tz(s):
+    try:
+        return pytz.timezone(s)
+    except:
+        msg = "Not a valid tz: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--api_id", required=False, help="user api ID", type=str, default=api_id)
+parser.add_argument("--api_hash", required=False, help="user api Hash", type=str, default=api_hash)
+parser.add_argument("--tz", required=False,  help="user api Hash", type=valid_tz, default=valid_tz('Asia/Tashkent'))
+
+args = parser.parse_args()
+
+client = TelegramClient("carpediem", args.api_id, args.api_hash)
 client.start()
 
 
@@ -18,7 +32,7 @@ async def main():
 
     while True:
         if time_has_changed(prev_update_time):
-            bts = generate_time_image_bytes(datetime.now())
+            bts = generate_time_image_bytes(datetime.now(args.tz).replace(tzinfo=None))
             await client(DeletePhotosRequest(await client.get_profile_photos('me')))
             file = await client.upload_file(bts)
             await client(UploadProfilePhotoRequest(file))
